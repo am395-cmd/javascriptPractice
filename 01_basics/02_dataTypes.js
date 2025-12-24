5e114640522c72f91ae481b24758d383d107edea
am395-cmd <amsubhan395@gmail.com>
2025-12-24 19:01:44 +0000

    data types file saved

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"user strict"; //for treat all old or new JS code as newer version
+
+//alert(3 + 3) // this not work here bcz we use nodejs, not browser
+
+console.log(3 
+    + 3) // code readability should be high
+
+console.log("Pakistan")
+
+
+let age = 20
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => not initialize
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Pakistan");
+
+console.log(typeof age);
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
