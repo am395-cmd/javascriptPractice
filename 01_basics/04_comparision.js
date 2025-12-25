244b41043c44cca8f847fc98fc3aedd8e79f235e
am395-cmd <amsubhan395@gmail.com>
2025-12-25 16:49:29 +0000

    comparision work done here

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+//comparisions
+
+// console.log(5 > 2);
+// console.log( 3 < 6);
+// console.log(9 >= 8);
+// console.log(5 <= 7);
+// console.log(4 == 4);
+// console.log(13 != 5);
+
+// comparision always same datatype k darmiyan hona chahiye otherwise issue create ho sakta hy
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+// in JS comparision and equality check are different 
+
+// in JS '==' is differ from '==='
+
+console.log("2" === 2); //false due to different datatypes
+
+console.log(2 === 2); //true due to same datatypes
