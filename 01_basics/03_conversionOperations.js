1197e28aaa78f5aa34b96aad7697c5d288f7d2fa
am395-cmd <amsubhan395@gmail.com>
2025-12-25 16:34:24 +0000

    operations updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,46 +1,86 @@
 //number datatype
-let score = 78
+// let score = 78
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 //changing datatypes
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
 //not a number data type
-let runs = "36abc"
+// let runs = "36abc"
 
-let valueInRuns = Number(runs);
+// let valueInRuns = Number(runs);
 
-console.log(typeof valueInRuns);
-console.log(valueInRuns);
+// console.log(typeof valueInRuns);
+// console.log(valueInRuns);
 
 //null datatype
-let goals = null
+// let goals = null
 
-let changeGoal = Number(goals);
+// let changeGoal = Number(goals);
 
-console.log(typeof changeGoal);
+// console.log(typeof changeGoal);
 
-console.log(changeGoal);
+// console.log(changeGoal);
 
 // "33" => 33
 //"33abc" => NaN (not a number)
 //true => 1
 //false => 0
 
-let isLoggedIn = 1;
+// let isLoggedIn = 1;
 
-let boolIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolIsLoggedIn);
+// let boolIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolIsLoggedIn);
 
 //1 => true
 //0  => false
 // "" =>false
 //"Pakistan" => true
 
-let numberSome = 45
+// let numberSome = 45
 
-let numberString = String(numberSome);
+// let numberString = String(numberSome);
 
-console.log(numberString);
-console.log(typeof numberString);
+// console.log(numberString);
+// console.log(typeof numberString);
+
+
+/* ********************************************** OPERATIONS ************************************************** */
+
+// console.log(2+2);
+// console.log(2*3);
+// console.log(5-2);
+// console.log(2**4);
+// console.log(6/2);
+// console.log(7%2);
+
+
+// let str1 = "Hello"
+// let str2 = "Pakistan"
+
+// let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("2" + 3);
+// console.log(3 + "2");
+// console.log("3" + 2 + 4);
+// console.log(3 + 2 + "4");
+
+// if string type in start next value convert into strings otherewise number values operation done but not with string values
+
+
+//console.log( 3 * 5 / 8 % 6); //not a good practise code
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2; //its a not good coding practise
+
+// let counter = 100
+
+// counter++; postfix
+// ++counter; //prefix
+// console.log(counter);
+
+// prefix update the value then assign and postfix assign the original value then update
