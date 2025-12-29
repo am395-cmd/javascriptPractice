f6623ad3bf2ef5d1b275968583fee6f06be59703
am395-cmd <amsubhan395@gmail.com>
2025-12-29 17:20:25 +0000

    dates

---
+++ 01_basics/08_DatesinJs.js
@@ -0,0 +1,26 @@
+//DATES
+
+// let myDate = new Date()
+// console.log(myDate);
+
+// console.log(myDate.toString());              //day, date with gmt
+// console.log(myDate.toDateString());         //date, day
+// console.log(myDate.toLocaleDateString());   //date, day with time
+// console.log(typeof myDate);                 //object
+
+// let specificDate = new Date(2024, 1, 1)   //specific date created with year, month, date
+
+// console.log(specificDate);
+
+// let mySpecificDate = new Date (1989, 1, 1, 9, 35)
+// console.log(mySpecificDate);
+// console.log(mySpecificDate.toLocaleDateString());
+// console.log(mySpecificDate.toDateString());
+
+// let checkDate = new Date("02-01-1989")       //day, month, date, year
+// console.log(checkDate.toDateString());
+// console.log(checkDate.toLocaleDateString());
+
+let checkTimeStamp = Date.now()
+
+console.log(checkTimeStamp);
